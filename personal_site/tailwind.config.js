075a3f5ab4fa91b50310d97e2f78852d6e079600
Dawid Mlynarski <dwdmlynarski@gmail.com>
2023-11-09 15:30:41 +0100

    animations

--- personal_site/tailwind.config.js
+++ personal_site/tailwind.config.js
@@ -4,8 +4,35 @@ module.exports = {
 		'./templates/**/*.html'
 	], 
 	theme: { 
-		extend: {}, 
+		extend: {
+			keyframes: { 
+				appear_indicator: {
+				'0%': {
+					opacity:'0'
+				},
+				'25%': {
+					opacity:'0'
+				},
+				'50%': {
+					opacity: '0'
+				},
+				'75%': {
+					opacity:'0'
+				},
+				'100%': {
+					opacity:'100'
+				}
+				}
+			
+			
+			},
+			animation: {
+				appear_indicator: 'appear_indicator 5s, bounce 1s infinite',
+			  }
+		}, 
 	}, 
 	plugins: [], 
 }
+
+
 
